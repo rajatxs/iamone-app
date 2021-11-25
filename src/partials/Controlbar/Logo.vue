da8706f2968c7d177068a5575685fc1ea8f57e4b
Rajat Sharma <rajat@developar.in>
2021-11-25 15:11:56 +0530

    Update logo image source

--- src/partials/Controlbar/Logo.vue
+++ src/partials/Controlbar/Logo.vue
@@ -1,19 +1,19 @@
+<template>
+   <div class="app-logo" aria-label="logo">
+      <img 
+         class="app-logo-image"
+         src="/icons/im1.svg"
+         :width="size"
+         :height="size"
+         alt="iamone" />
+   </div>
+</template>
+
 <script setup lang="ts">
 import { defineProps } from 'vue'
 
 defineProps<{ size?: number }>()
 </script>
-
-<template>
-   <div class="app-logo" aria-label="logo">
-      <img 
-         class="app-logo-image"
-         src="../../assets/logo.svg"
-         :width="size"
-         :height="size"
-         alt="im1" />
-   </div>
-</template>
 
 <style lang="scss" scoped>
 .app-logo {
