6112101caa21068c4a10f1338fc2ed9ebcd0f066
Rajat Sharma <rxx175@gmail.com>
2022-01-23 16:36:29 +0530

    Integrate EmailVerificationModal

--- src/partials/UniversalArea.vue
+++ src/partials/UniversalArea.vue
@@ -7,6 +7,10 @@
       <share-link-modal
          v-if="viewShareModal"
          @close="SHOW_SHARE_MODAL(false)" />
+
+      <email-verification-modal
+         v-if="viewEmailVerificationModal"
+         @close="SHOW_EMAIL_VERIFICATION_MODAL(false)" />
    </div>
 </template>
 
@@ -15,6 +19,7 @@ import Vue from 'vue'
 import { createNamespacedHelpers } from 'vuex'
 import QRCodeModal from './Modals/QRCodeModal.vue'
 import ShareLinkModal from "./Modals/ShareLinkModal.vue"
+import EmailVerificationModal from "./Modals/EmailVerificationModal.vue";
 
 const { 
    mapState: mapUIState, 
@@ -26,18 +31,21 @@ export default Vue.extend({
    components: {
       'qrcode-modal': QRCodeModal,
       'share-link-modal': ShareLinkModal,
+      'email-verification-modal': EmailVerificationModal
    },
    computed: mapUIState([
       'viewAuthModal',
       'viewQRCodeModal',
       'viewShareModal',
-      'viewUpdatePasswordModal'
+      'viewUpdatePasswordModal',
+      'viewEmailVerificationModal'
    ]),
    methods: mapUIMutations([
       'SHOW_AUTH_MODAL',
       'SHOW_QRCODE_MODAL',
       'SHOW_SHARE_MODAL',
-      'SHOW_UPDATE_PASSWORD_MODAL'
+      'SHOW_UPDATE_PASSWORD_MODAL',
+      'SHOW_EMAIL_VERIFICATION_MODAL'
    ])
 })
 </script>
