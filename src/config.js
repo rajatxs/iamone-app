2a67ea0a3e508777cfe0010dd6952a9aeeaa9340
Rajat Sharma <rxx175@gmail.com>
2022-03-20 00:32:07 +0530

    Added BASE_URL

--- src/config.js
+++ src/config.js
@@ -3,3 +3,4 @@ export const REMOTE_SERVER_URL = IS_PROD? "https://api.iamone.link": "http://loc
 export const REMOTE_API_SERVER_URL = REMOTE_SERVER_URL + "/x/api";
 export const REMOTE_THEME_SOURCE_URL = REMOTE_SERVER_URL + "https://www.iamone.link/x/themes";
 export const STATIC_RESOURCE_URL = "https://static.iamone.link";
+export const BASE_URL = "https://www.iamone.link"
